a5b4bc5db5d9bc2ab06915c928f862c2183d6477
Dzianis Maroz <kapitan.frost@gmail.com>
2024-07-24 19:30:43 +0300

    <ttempt #1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module stepik/8/99_hw/i2s
+
+go 1.22.5
--- i2s.go
+++ i2s.go
@@ -1,7 +1,45 @@
 package main
 
-/*
+import (
+	"fmt"
+	"reflect"
+)
+
+// Function to fill struct fields with values of map.
+// @params :
+// data - map[string]interface{}
+// out - struct{}
 func i2s(data interface{}, out interface{}) error {
-	// todo
+	dataType := reflect.TypeOf(data)
+	dataVal := reflect.ValueOf(data)
+	outType := reflect.TypeOf(out).Elem()
+	outVal := reflect.ValueOf(out).Elem()
+
+	// tempMap := map[string]interface{}{}
+
+	if dataType.Kind() != reflect.Map || outType.Kind() != reflect.Struct {
+		return fmt.Errorf("invalid types provided: %v, %v", dataType.Kind(), outType.Kind())
+	}
+
+	fmt.Println("we got: ")
+	fmt.Println("data:", dataType.Kind(), "out:", outType.Kind(), "name:", outType.Name())
+	for _, e := range dataVal.MapKeys() {
+		f := outVal.FieldByName(e.String())
+		if !f.IsValid() || !f.CanSet() {
+			return fmt.Errorf("unable to set field  %s", e.String())
+		}
+		val := dataVal.MapIndex(e)
+		switch f.Kind() {
+		case reflect.String:
+			f.SetString(val.String())
+		case reflect.Int, reflect.Float64:
+			f.SetInt(val.Int())
+		case reflect.Bool:
+			f.SetBool(val.Bool())
+		}
+
+		// tempMap[e.String()] = dataVal.MapIndex(e).Interface()
+	}
+
+	return nil
 }
-*/
